0f6ac6fda331cb87a305fa7307448417bf179573
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-12-19 09:36:27 +0200

    The new v-model

---
+++ src/components/InputComp.vue
@@ -0,0 +1,49 @@
+<template>
+<!--  vue 2-->
+<!--  <input
+    :value="email"
+    :type="type"
+    @input="$emit(
+      'input',
+      $event.target.value
+    )"
+  >-->
+<!--  vue 3-->
+    <input
+      :value="modelValue"
+      :type="type"
+      @input="$emit(
+        'update:modelValue',
+        $event.target.value
+      )"
+    >
+</template>
+
+<script>
+export default {
+  name: 'InputComp',
+  data() {
+    return {
+      type: 'input'
+    }
+  },
+  //vue 2
+  /*props: {
+    email: {
+      type: [String, Number],
+      default: ''
+    }
+  }*/
+  //vue 3
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
